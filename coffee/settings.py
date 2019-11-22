7f65cbb9c5a6bc11f38e9152056335febce2606f
alison <alison@metanoia>
2019-11-21 21:29:29 -0300

    Extracted SECRET_KEY setting
    close #24

--- coffee/settings.py
+++ coffee/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yan1m!0yt186ce(g-6az+!_y6!pd_^a)%or&h7ik)chz73fdl='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
