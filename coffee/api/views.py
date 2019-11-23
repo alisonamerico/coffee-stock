9f382fe2c947a84e317d672d8e85040bd3d302e8
alison <alison@metanoia>
2019-11-22 21:52:37 -0300

    Created views
    close #35

--- coffee/api/views.py
+++ coffee/api/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+
+from coffee.api.models import Harvest, Stock, Farm
 
 
-def home(request):
-    return HttpResponse("Hello World!")
+class HarvestViewSet(viewsets.ModelViewSet):
+    queryset = Harvest.objects.all()
+    # serializer_class = HarvestSerializer
+
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    # serializer_class = StockSerializer
+
+
+class FarmViewSet(viewsets.ModelViewSet):
+    queryset = Farm.objects.all()
+    # serializer_class = FarmSerializer
