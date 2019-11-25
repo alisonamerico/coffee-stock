a3bcf9ff7aadff851c80ef493ed8ddf33f0cb158
alison <alison@metanoia>
2019-11-25 14:01:39 -0300

    Created Authentication
    close #49

--- coffee/api/serializers.py
+++ coffee/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import ListField
 
 from coffee.api.models import Harvest, Stock
 
@@ -9,7 +10,24 @@ class HarvestSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class StringArrayField(ListField):
+    """
+    String representation of an array field.
+    """
+    def to_representation(self, obj):
+        obj = super().to_representation(self, obj)
+        # convert list to string
+        return ",".join([str(element) for element in obj])
+
+    def to_internal_value(self, data):
+        data = data.split(",")  # convert string to list
+        return super().to_internal_value(self, data)
+
+
 class StockSerializer(serializers.ModelSerializer):
+    cafes_types = StringArrayField()
+    coffee_farms = StringArrayField()
+
     class Meta:
         model = Stock
         fields = '__all__'
